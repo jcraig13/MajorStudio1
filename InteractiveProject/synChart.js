ebb94b6f20d270cc9e0ff33acb696e7ae8f33644
jcraig13 <48970337+jcraig13@users.noreply.github.com>
2021-12-07 01:40:55 -0500

    added charts

--- InteractiveProject/synChart.js
+++ InteractiveProject/synChart.js
@@ -1,32 +1,21 @@
-const gra = function(min, max) {
-    return Math.random() * (max - min) + min;
-}
-const init = function(){
-	let items = document.querySelectorAll('section');
-	for (let i = 0; i < items.length; i++){
-		items[i].style.background = "-webkit-linear-gradient(left, #527e8b 50%,#f3dac4 50%,#f3dac4 50%)";
-	}
-	cssScrollSnapPolyfill()
-}
-init();
 
 //natural chart
 // set the dimensions and margins of the graph
-var margin = {top: 15, right: 20, bottom: 20, left: 20},
-width = 720 - margin.left - margin.right,
-height = 900 - margin.top - margin.bottom;
+var margin2 = {top: 15, right: 20, bottom: 20, left: 20},
+width = 720 - margin2.left - margin2.right,
+height = 800 - margin2.top - margin2.bottom;
 
 // append the svg object to the body of the page
-var svg = d3.select("#natural_chart")
+var svg2 = d3.select("#synthetic_chart")
 .append("svg")
-.attr("width", width + margin.left + margin.right)
-.attr("height", height + margin.top + margin.bottom)
+.attr("width", width + margin2.left + margin2.right)
+.attr("height", height + margin2.top + margin2.bottom)
 .append("g")
 .attr("transform",
-      "translate(" + margin.left + "," + margin.top + ")");
+      "translate(" + margin2.left + "," + margin2.top + ")");
 
 // Parse the Data
-d3.csv("./data/naturalMaterial.csv", function(data) {
+d3.csv("./data/syntheticMaterial.csv", function(data) {
 
 // List of subgroups = header of the csv files
 var subgroups = data.columns.slice(1)
@@ -43,22 +32,22 @@ console.log(subgroups)
       .domain(groups)
       .range([0, width])
       .padding([0.2])
-  // svg.append("g")
-  //   .attr("transform", "translate(0," + height + ")")
-  //   .call(d3.axisBottom(x).tickSizeOuter(0));
+//   svg2.append("g")
+//     .attr("transform", "translate(0," + height + ")")
+//     .call(d3.axisBottom(x).tickSizeOuter(0));
 
 
 // Add Y axis
 var y = d3.scaleLinear()
-.domain([0, 300])
+.domain([0, 1800])
 .range([ height, 0 ]);
-svg.append("g")
+// svg2.append("g")
 // .call(d3.axisLeft(y));
 
 // color palette = one color per subgroup
 var color = d3.scaleOrdinal()
 .domain(subgroups)
-.range(['rgba(243,218,196,1)','rgba(243,218,196,.66)','rgba(243,218,196,.33)'])
+.range(['rgba(82,126,139,1)','rgba(82,126,139,.66)','rgba(82,126,139,.33)'])
 
 //stack the data? --> stack per subgroup
 var stackedData = d3.stack()
@@ -66,7 +55,7 @@ var stackedData = d3.stack()
 (data)
 
   // Show the bars
-  svg.append("g")
+  svg2.append("g")
     .selectAll("g")
     // Enter in the stack data = loop key per key = group per group
     .data(stackedData)
@@ -83,14 +72,9 @@ var stackedData = d3.stack()
 })
 
 //rotates the chart 90 degrees to the left
-document.querySelector("#natural_chart").style.transform = 'rotate(-90deg)';
+document.querySelector("#synthetic_chart").style.transform = 'rotate(90deg)';
 // document.querySelector("#natural_chart").style.position = 'absolute';
 // document.querySelector("#natural_chart").style.marginLeft = '0';
-document.querySelector("#natural_chart").style.float = 'right';
-document.querySelector("#natural_chart").style.paddingTop = '63px';
-
-
-
-
-
-
+document.querySelector("#synthetic_chart").style.float = 'left';
+// document.querySelector("#synthetic_chart").style.width = '50%';
+document.querySelector("#synthetic_chart").style.paddingBottom = '37px';
